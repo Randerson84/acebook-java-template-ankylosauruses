610cdff505cc3d1dbfc19d84de867a020d714819
James <jahalliday16@googlemail.com>
2023-04-20 17:30:31 +0100

    prod properties changed

--- src/main/java/com/makersacademy/acebook/Application.java
+++ src/main/java/com/makersacademy/acebook/Application.java
@@ -1,11 +1,13 @@
 package com.makersacademy.acebook;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 //@ComponentScan(basePackages ={""})
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
