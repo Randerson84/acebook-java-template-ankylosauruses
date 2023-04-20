daf4a797dc4714ef9f01d15b4d15edd6f88afbc5
James <jahalliday16@googlemail.com>
2023-04-20 17:42:14 +0100

    prod properties changed

--- src/main/java/com/makersacademy/acebook/Application.java
+++ src/main/java/com/makersacademy/acebook/Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 //@ComponentScan(basePackages ={""})
 @SpringBootApplication
-@EnableAutoConfiguration
+@ComponentScan(basePackages ={""})
 public class Application {
 
     public static void main(String[] args) {
