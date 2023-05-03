0c8534d8cb24656decbcb07d41f5c007bbc814f0
Nathan Welsh <nathanwelsh.jobs@gmail.com>
2023-05-03 12:01:01 +0100

    ugly-comments

--- src/main/java/com/makersacademy/acebook/controller/CommentController.java
+++ src/main/java/com/makersacademy/acebook/controller/CommentController.java
@@ -1,13 +1,15 @@
 package com.makersacademy.acebook.controller;
 
 import com.makersacademy.acebook.model.Comment;
+import com.makersacademy.acebook.model.Post;
 import com.makersacademy.acebook.repository.CommentRepository;
 import com.makersacademy.acebook.repository.UserRepository;
-
+import com.makersacademy.acebook.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -17,12 +19,16 @@ public class CommentController {
     CommentRepository commentRepository;
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    PostRepository postRepository;
 
-    @PostMapping("/comments")
-    public RedirectView create(@ModelAttribute Comment comment, Authentication auth) {
+    @PostMapping("/comments/{id}")
+    public RedirectView create(@ModelAttribute Comment comment, Authentication auth, @PathVariable("id") Long id) {
         String username = auth.getName() ;
         Long userId = userRepository.findByUsername(username).get(0).getId();
         comment.setUsersid(userId);
+//        Post post = postRepository.findById(id).get();
+     comment.setPostsid(id);
         commentRepository.save(comment);
         return new RedirectView("/posts");
     }
